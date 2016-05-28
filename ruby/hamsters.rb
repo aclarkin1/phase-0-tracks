ccd1d4ccab0e2a485b4677501b2039bb7f93e779
Aaron Larkin <aclarkin1@gmail.com>
2016-05-28 00:53:13 +0000

    Update volume and age to gets.to_i insteada of gets.chomp

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,13 +1,13 @@
 puts "What is the hamster's name?"
   name = gets.chomp
 puts "How loud is the #{name} on a scale of 1-10? (10 being the loudest)"
-  volume = gets.chomp
+  volume = gets.to_i
 puts "What color is #{name}'s hair?"
   hair_color = gets.chomp
 puts "Is #{name} a good candidate for adoption?"
   eligiblity = gets.chomp
 puts "About how old is #{name}?"
-  age = gets.chomp
+  age = gets.to_i
 
 if age == ""
   age = nil
