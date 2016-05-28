238723310fb1c832662f33a64e63019343a9d2ff
Aaron Larkin <aclarkin1@gmail.com>
2016-05-28 19:36:21 +0000

    Add case statement to test if dracula

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,11 +1,23 @@
 puts "What is your name"
   name = gets.chomp
-puts "How old are you?
-  age = gets.chomp
+puts "How old are you?"
+  age = gets.to_i
 puts "What year were you born?"
   dob = gets.to_i
 puts "Our company serves garlic bread. Should we order some for you?"
   bread = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
   insurance = gets.chomp
-  
+
+case
+when (name == "Drake Cula" || name == "Tu Fang")
+  puts "Definitely a Vampire"
+when insurance == "Yes" && bread == "Yes"
+  puts 'Probably not a Vampire'
+when insurance == "No" && bread == "No"
+  puts 'Almost certainly a Vampire.'
+when insurance == "No" || bread == "No"
+  puts 'Probably a Vampire.'
+else
+  puts 'More information needed.'
+end
