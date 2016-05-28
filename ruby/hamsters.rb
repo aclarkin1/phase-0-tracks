6c56e04a734ef2187ed4a75f6c529ffddd36cd26
Aaron Larkin <aclarkin1@gmail.com>
2016-05-28 00:49:37 +0000

    Add code corresponding to DBC 4.3, Release 1

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,2 +1,18 @@
 puts "What is the hamster's name?"
-  input = gets.chomp
+  name = gets.chomp
+puts "How loud is the #{name} on a scale of 1-10? (10 being the loudest)"
+  volume = gets.chomp
+puts "What color is #{name}'s hair?"
+  hair_color = gets.chomp
+puts "Is #{name} a good candidate for adoption?"
+  eligiblity = gets.chomp
+puts "About how old is #{name}?"
+  age = gets.chomp
+
+if age == ""
+  age = nil
+end  
+
+if age == nil
+  puts "#{name}'s age is unknown."
+end
