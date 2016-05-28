c8e7089dfeac97994221420aa7975a77f355c1b3
Aaron Larkin <aclarkin1@gmail.com>
2016-05-28 12:07:07 +0000

    Add puts commands for each question

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What is your name"
+  name = gets.chomp
+puts "How old are you?
+  age = gets.chomp
+puts "What year were you born?"
+  dob = gets.to_i
+puts "Our company serves garlic bread. Should we order some for you?"
+  bread = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+  insurance = gets.chomp
+  
