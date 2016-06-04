3914ecd574e7f62a5a5415e8a49f4be7b57dca6e
Aaron Larkin <aclarkin1@gmail.com>
2016-06-04 20:03:38 +0000

    Add methods using '.each' and '.map' to iterate Liverpool players' shirt numbers

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,3 +6,13 @@ def french_open
 end
   
 french_open { |title1, name1| puts "The winner of the French Open 2016 in #{title1} is #{name1}" }
+
+jersey_number = {"Emre Can" => 6, "Luis Suarez" => 7, "Steven Gerrard" => 8, "Fernando Torres" => 9}
+
+jersey_number.each do |players, shirt|
+  puts "#{players} wore the number #{shirt} for Liverpool"
+end
+
+jersey_number.map do |players, shirt|
+  puts "In an upcoming charity match, #{players} will instead wear number #{shirt.next}"
+end
