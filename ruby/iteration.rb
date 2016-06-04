b43854ae10c81004eb3d3a99ec1f97e00261c3c2
Aaron Larkin <aclarkin1@gmail.com>
2016-06-04 20:42:12 +0000

    Add method calls to player/shirt hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -16,3 +16,19 @@ end
 jersey_number.map do |players, shirt|
   puts "In an upcoming charity match, #{players} will instead wear number #{shirt.next}"
 end
+
+jersey_number = {"Emre Can" => 6, "Luis Suarez" => 7, "Steven Gerrard" => 8, "Fernando Torres" => 9}
+
+# A method that iterates through the items, deleting any that meet a certain condition (for example, deleting any numbers that are less than 5).
+jersey_number.delete_if{|player, shirt| shirt >= 8 }
+
+# A method that filters a data structure for only items that do satisfy a certain condition (for example, keeping any numbers that are less than 5).
+jersey_number.keep_if{|player, shirt| shirt >=8 }
+
+# A different method that filters a data structure for only items satisfying a certain condition
+
+jersey_number.select{|player, shirt| player.include?("Steven")}
+
+# A method that will remove items from a data structure until the condition in the block evaluates to false, then stops.
+
+jersey_number.reject{|player, shirt| shirt < 8}
