30dda627b41dbc3413af2f46d4377fd3b71a54f6
Aaron Larkin <aclarkin1@gmail.com>
2016-06-10 00:32:14 +0000

    Add speak, eat_milk_and_cookies and initialize methods

---
+++ ruby/santa.rb
@@ -0,0 +1,16 @@
+def speak
+  puts "Ho, ho, ho! Haaaappy holidays!"
+end
+
+def eat_milk_and_cookies(cookie_type)
+  puts "That was a good #{cookie_type}!"
+end
+
+def initialize
+  puts "Initializing Santa instance..."
+end
+
+initialize
+
+#eat_milk_and_cookies("snickerdoodle")
+#speak
