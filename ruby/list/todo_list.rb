e70e52d09663a826da7fa1b89f16e9f96493ce3d
Aaron Larkin <aclarkin1@gmail.com>
2016-06-11 22:47:37 +0000

    Add methods corresponding to todo_list_spec.rb

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,22 @@
+class TodoList
+  def initialize(x)
+    @x = x
+  end
+  
+  def get_items
+   @x
+  end
+  
+  def add_item(y)
+    @x << y
+  end
+  
+  def delete_item(z)
+    @x.delete(z)
+  end
+  
+  def get_item(w)
+    @x.at(w)
+  end
+  
+end
